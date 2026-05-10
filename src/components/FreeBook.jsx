c59a78f43977eb9e80e8ac8503b7be8afab3e933
Gaurav jha <jhagaurav214@gmail.com>
2026-05-10 12:17:32 +0530

    changes done

--- src/components/FreeBook.jsx
+++ src/components/FreeBook.jsx
@@ -3,47 +3,65 @@ import "slick-carousel/slick/slick-theme.css";
 import SliderLib from "react-slick";
 const Slider = SliderLib.default;
 import Card from "./Card";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 const FreeBook = () => {
   const [book, setBook] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const getData = async () => {
-      const res = await axios.get("https://book-store-backend-sigma.vercel.app/book");
+      try{
+        const res = await axios.get("https://book-store-backend-sigma.vercel.app/book");
       const data = res.data.filter((list) => list.price === 0);
       setBook(data);
+
+      }
+      catch(error){
+        console.log(error);
+
+      }
+      finally{
+         setLoading(false);
+      }
+      
     }
     getData();
 
   }, [])
-  var settings = {
+var settings = {
   dots: true,
-  infinite: false,
+  infinite: true,
   speed: 500,
+
   slidesToShow: 3,
-  slidesToScroll: 3,
+  slidesToScroll: 1,
+
+  adaptiveHeight: false,
 
   responsive: [
     {
       breakpoint: 1024,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2,
-      }
+      },
     },
 
     {
       breakpoint: 768,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1,
-        initialSlide: 0
-      }
-    }
-  ]
+      },
+    },
+  ],
 };
+ if (loading) {
+  return (
+    <div className="text-center py-10">
+      Loading...
+    </div>
+  );
+}
   return <>
     <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 ">
       <div>
